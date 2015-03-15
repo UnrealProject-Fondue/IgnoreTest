63fd7b86a9def61c15cf6033d9493502236e30d2
sellnat77 <sell_nat@yahoo.com>
2015-03-14 20:39:43 -0700

    Added two more classes and a new map with a cone

---
+++ IgnoreTest/Source/IgnoreTest/MultiMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MultiMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IGNORETEST_API AMultiMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ IgnoreTest/Source/IgnoreTest/Multiplayer.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Multiplayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IGNORETEST_API AMultiplayer : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
