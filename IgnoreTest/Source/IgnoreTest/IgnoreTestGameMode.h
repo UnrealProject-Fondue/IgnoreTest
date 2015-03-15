7c4397c49cd278f76c3d3a66cb9569e267d1d846
sellnat77 <sell_nat@yahoo.com>
2015-03-14 19:23:50 -0700

    Moment of truf

---
+++ IgnoreTest/Source/IgnoreTest/IgnoreTestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "IgnoreTestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IGNORETEST_API AIgnoreTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
