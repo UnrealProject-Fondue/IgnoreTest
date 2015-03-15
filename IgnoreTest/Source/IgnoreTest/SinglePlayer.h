43db2ece01b35d2d2f9bc4f814697ad27e72fbd3
Russell Tan <sell_nat@yahoo.com>
2015-03-14 20:19:00 -0700

    Build pulled, compiled, added class for client 2

---
+++ IgnoreTest/Source/IgnoreTest/SinglePlayer.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SinglePlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IGNORETEST_API ASinglePlayer : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
